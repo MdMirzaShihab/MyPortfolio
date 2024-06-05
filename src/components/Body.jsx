9a0f392919966db897755d04b69f7b458f4ae438
MdMirzaShihab <msmd2@graduate.utm.my>
2024-06-05 09:47:13 +0600

    body modified

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,9 +3,13 @@ import CSM from '../assets/CSM-logo.png';
 const Body = () => {
   return (
     <div>
-        <h1 className='h-[700px] bg-[#060f30]'>
-            2nd portion
-            <img src={CSM} className="object-cover object-top" alt="CSM" />
+        <h1 className=' relative bg-[#060f30] h-[700px]'>
+          <div className='absolute flex w-80 h-96 p-2 top-3 right-5 bg-white justify-center items-center rounded-2xl border-4 border-sky-200'>
+          <img src={CSM} className=" object-contain " alt="CSM" />
+          </div>
+          <div className='absolute flex w-80 h-96 p-2 top-5 right-8 bg-white justify-center items-center rounded-2xl border-4 border-sky-200'>
+          <img src={CSM} className=" object-contain " alt="CSM" />
+          </div>
         </h1>
     </div>
   )
