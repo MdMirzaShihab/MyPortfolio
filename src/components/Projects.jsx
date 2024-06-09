ff1c9cc54dd6a360ae8d84b5cfd9a7292fbafabf
Md Mirza Shihab <mirzashihab@gmail.com>
2024-06-09 08:46:56 -0700

    Added video and a new section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div className='h-screen bg-slate-500'>
-      <h1 className='w-full text-center text-6xl font-bold text-white'>Projects</h1> </div>
+    <div className='relative h-screen bg-slate-500'>
+<div data-scroll data-scroll-speed="0.2">
+        <h1 className="badgeText font-bold text-5xl md:text-8xl pt-10 text-center pb-5 text-white">
+        Projects
+        </h1>
+      </div>
+        
+         </div>
     
   )
 }
